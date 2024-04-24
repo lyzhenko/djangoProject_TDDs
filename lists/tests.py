79e2fc35efdf123827af527fd0b276325f904683
Александр Лыженко <alex@lyzhenko.ru>
2024-04-24 19:04:05 +0800

    Добавлено приложение для списков с намеренно неработающим модульным тестом

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SmokeTest(TestCase):
+    """ Тест на токсичность """
+
+    def test_bad_math(self):
+        self.assertEqual(1 + 1, 3)
